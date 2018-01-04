f758e1684b66ea7a3d2043ea462ff492723075de
test <test>
2018-01-04 16:24:50 +0800

    initail commit

---
+++ common-pay/common-pay-web/src/main/java/com/fredchen/web/CommonPayWebApplication.java
@@ -0,0 +1,12 @@
+package com.fredchen.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonPayWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonPayWebApplication.class, args);
+	}
+}
